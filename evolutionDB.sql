a3e745a5edbb8ced0c38f4426cd824bd682b69a4
adriano-klein <adriano25klein@gmail.com>
2019-01-18 17:01:39 -0200

    Inserindo o banco de Dados

--- evolutionDB.sql
+++ evolutionDB.sql
@@ -29,7 +29,7 @@ CREATE TABLE `cadastro` (
   `email` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,9 +38,39 @@ CREATE TABLE `cadastro` (
 
 LOCK TABLES `cadastro` WRITE;
 /*!40000 ALTER TABLE `cadastro` DISABLE KEYS */;
-INSERT INTO `cadastro` VALUES (12,'Adriano','Klein','adriano@gmail.com','123456'),(13,'Bruno','Ferreira','bruno@gmail.com','246810'),(14,'Adriano','Ono','ono@gmail.com','abcdef');
+INSERT INTO `cadastro` VALUES (12,'Adriano','Klein','adriano@gmail.com','123456'),(13,'Bruno','Ferreira','bruno@gmail.com','246810'),(14,'Adriano','Ono','ono@gmail.com','abcdef'),(15,'Leandro','Nardoni','nardoni@email.com','$2y$10$FT6og9DP.nQSOWZRQRzQy.Ps6a0Kq2b5g0TE4kjpXyGLHGstS8T8q'),(16,'Adriano','Ono','ono@gmail.com','$2y$10$4zXkMjMVXkhiA.bdNwrBVOqiaxoAeE8T3hxBot0NvaKppHK9v6Hay'),(17,'Adriano','Ono','ono@gmail.com','$2y$10$8MU6VtMxaNKn81jj1tWIeOWGcUpMyVXcrsztKSThg8f2ZL/0Jjkci'),(18,'Leandro','Karnal','Karnal@email.com','$2y$10$Z3zpG9ZAK9NYGg0XwKi02.WoQGmQhsK3jn9Yoa9LBVunQcGl1Bj76'),(19,'Gisele','Silveira','silveira@email.com','$2y$10$7RsZeoIpFJTtnAtFByhr2e0G7pyZ54Ydjb/dK7GGzNU4xTROTF5Bq');
 /*!40000 ALTER TABLE `cadastro` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `newsletter`
+--
+
+DROP TABLE IF EXISTS `newsletter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `newsletter` (
+  `email` varchar(255) NOT NULL,
+  `data` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `newsletter`
+--
+
+LOCK TABLES `newsletter` WRITE;
+/*!40000 ALTER TABLE `newsletter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newsletter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'evolution'
+--
+
+--
+-- Dumping routines for database 'evolution'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-15 10:44:56
+-- Dump completed on 2019-01-18 16:58:34
