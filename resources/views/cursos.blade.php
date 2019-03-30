29e7988053cb97c3fe14c4efbc06720c5c880e08
Bruno-Goudric <brunolfsports@hotmail.com>
2019-03-29 21:54:00 -0300

    ajuste na tela de cursos

--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -22,7 +22,7 @@
 						</figure>
 						<div class="wrapper">
 							<small>{{$curso->categorias->nome}}</small>
-							<h3> {{ $curso->nome }} </h3>
+							<h3 class="titulos"> {{ $curso->nome }} </h3>
 							<p> {{ $curso->descricao }} </p>
 							<div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i> <small>(145)</small></div>
 						</div>
